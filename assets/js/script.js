567c52897bd93cb9e3807e985c8a35becfb6741b
Evita Orrock <evita.orrock@gmail.com>
2024-02-19 14:18:04 +0000

    Add docstrings to each of the functions to note intended functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,77 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
 })
 
-function gameSetup () {
+/**
+ * This function randomly chooses four colours and their positions in the code line up, and clears the guess and feedback rows. It resets the round counter.
+ */
+function gameSetup() {
 
 }
 
-function userSubmission () {
+/**
+ * This function submits the user's input and locks it in, preventing further changes.
+ */
+function userSubmission() {
 
 }
 
-function checkUserSubmission () {
+/**
+ * This function checks the four user-inputted guesses against the four computer-generated choices made at the start of the game in the gameSetup function. 
+ * It provides the result of how many of the guesses were a) correct colour and in the correct place, b) correct colour in the incorrect place and c) not a
+ * correct colour.
+ */
+function checkUserSubmission() {
 
 }
 
-function provideFeedback () {
+/**
+ * This function takes the output of the checkUserSubmission function and translates this into the number of red, white and null pegs should be presented for 
+ * that row of guesses in the feedback section of the game. 
+ */
+function provideFeedback() {
 
 }
 
-function advanceRound () {
+/**
+ * This function advances the round counter by one and provides a new row for guesses, pushing the previous row down the page.
+ */
+function advanceRound() {
 
 }
 
-function endOrContinue () {
+/**
+ * This function checks the round counter and if it is <10, it calls the advanceRound function to set up for a new round. Otherwise it ends the game with the
+ * correct end game message, according to whether they won or lost and if they lost, depending on how many they got correct.
+ */
+function endOrContinue() {
 
 }
 
-function undoChoice () {
+/**
+ * This function allows the user to remove the colour of the latest choice in the input row. 
+ */
+function undoChoice() {
 
 }
 
-function resetRow () {
+/**
+ * This function allows the user to remove the colours in the whole input row, however many have been entered so far. 
+ */
+function resetRow() {
 
 }
 
-function showRules () {
+/**
+ * This function shows the Rules modal box.
+ */
+function showRules() {
 
 }
 
-function showAbout () {
-    
+/**
+ * This function shows the About modal box. 
+ */
+function showAbout() {
+
 }

a0f0ba82daf713e350379e51c495dbef0d5c8555
Evita Orrock <evita.orrock@gmail.com>
2024-02-19 14:07:10 +0000

    Add basic JavaScript function skeletons

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+})
+
+function gameSetup () {
+
+}
+
+function userSubmission () {
+
+}
+
+function checkUserSubmission () {
+
+}
+
+function provideFeedback () {
+
+}
+
+function advanceRound () {
+
+}
+
+function endOrContinue () {
+
+}
+
+function undoChoice () {
+
+}
+
+function resetRow () {
+
+}
+
+function showRules () {
+
+}
+
+function showAbout () {
+    
+}
